c4017d51f4b9c1dcf56c32ed35ff002991875d15
Vanntph43399 <145000089+Vann0331@users.noreply.github.com>
2023-11-30 00:24:42 +0700

    van model KhuyenMa 30-11

--- src/MODEL/KhuyenMai.java
+++ src/MODEL/KhuyenMai.java
@@ -1,20 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package MODEL;
+
 import java.util.Date;
+
+/**
+ *
+ * @author Admin
+ */
 public class KhuyenMai {
+//van2811
     private int IdKM;
     private String MaKM;
     private String TenKM;
     private Date NgayBatDau;
     private Date NgayKetThuc;
-    private  String DieuKien;
+    private String DieuKien;
     private Double TienGiam;
-    private Boolean TrangThai;
-    private SanPham IdSp;
+    private String GhiChu;
+    private boolean TrangThai;
+    private SanPham IdSP;
+    
+    
 
     public KhuyenMai() {
     }
-
-    public KhuyenMai(int IdKM, String MaKM, String TenKM, Date NgayBatDau, Date NgayKetThuc, String DieuKien, Double TienGiam, Boolean TrangThai, SanPham IdSp) {
+//van2811
+    public KhuyenMai(int IdKM, String MaKM, String TenKM, Date NgayBatDau, Date NgayKetThuc, String DieuKien, Double TienGiam, String GhiChu, boolean TrangThai, SanPham IdSP) {
         this.IdKM = IdKM;
         this.MaKM = MaKM;
         this.TenKM = TenKM;
@@ -22,8 +36,9 @@ public class KhuyenMai {
         this.NgayKetThuc = NgayKetThuc;
         this.DieuKien = DieuKien;
         this.TienGiam = TienGiam;
+        this.GhiChu = GhiChu;
         this.TrangThai = TrangThai;
-        this.IdSp = IdSp;
+        this.IdSP = IdSP;
     }
 
     public int getIdKM() {
@@ -82,26 +97,29 @@ public class KhuyenMai {
         this.TienGiam = TienGiam;
     }
 
-    public Boolean getTrangThai() {
+    public String getGhiChu() {
+        return GhiChu;
+    }
+
+    public void setGhiChu(String GhiChu) {
+        this.GhiChu = GhiChu;
+    }
+
+    public boolean isTrangThai() {
         return TrangThai;
     }
 
-    public void setTrangThai(Boolean TrangThai) {
+    public void setTrangThai(boolean TrangThai) {
         this.TrangThai = TrangThai;
     }
 
-    public SanPham getIdSp() {
-        return IdSp;
+    public SanPham getIdSP() {
+        return IdSP;
     }
 
-    public void setIdSp(SanPham IdSp) {
-        this.IdSp = IdSp;
+    public void setIdSP(SanPham IdSP) {
+        this.IdSP = IdSP;
     }
+    
 
-    @Override
-    public String toString() {
-        return TenKM ;
-    }
-
-   
 }
